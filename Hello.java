0de15886c78467bd5dfeca7cebffaadfebfd57a8
rajanmagratitsk <magratirjn9815@gmail.com>
2026-02-28 12:42:13 +0545

    new task

---
+++ Hello.java
@@ -0,0 +1,16 @@
+// string concatenation
+import java.util.Scanner;
+public class Hello {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String n1;
+        String n2;
+        System.out.print("enter n1:");
+         n1=sc.next();
+        System.out.print("enter n2:");
+       
+        n2=sc.next();
+        System.out.println("together:"+n1.concat(n2));
+    }
+    
+}
