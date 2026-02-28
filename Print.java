e3caeb8cf9c6ac86475c56adbf373b3be83c0c85
rajanmagratitsk <magratirjn9815@gmail.com>
2026-02-28 13:46:53 +0545

    add

--- Print.java
+++ Print.java
@@ -2,6 +2,9 @@ public class Print {
     public static void main(String[] args) {
         for(int i =1;i<=5;i++){
             System.out.println(i);
+           
+            }System.out.println("done");
+
         }
     }
-}
+
